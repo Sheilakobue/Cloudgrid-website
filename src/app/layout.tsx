bfc466ca26cd9dec938ec0e907d6c300c435728b
Sheila Kobue <Maserole.mojela@gmail.com>
2024-05-14 13:45:13 +0200

    created and deleted file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from "next";
 import {Poppins} from "next/font/google";
+import Navbar from "./navbar/page";
 
 const poppins = Poppins({subsets: ["latin"], 
 weight: [
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className ={poppins.className}>
-           
+          <Navbar/>
         {children}</body>
     </html>
   )
