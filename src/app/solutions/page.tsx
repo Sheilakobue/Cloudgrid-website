84ac396ccc541925add6e3310bf4ee47cf97fd59
Sheila Kobue <Maserole.mojela@gmail.com>
2024-05-14 11:38:07 +0200

    Create page.tsx

---
+++ src/app/solutions/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function SolutionList() {
+  return (
+    <>
+    <h1>Our Solutions</h1>
+    <h2>Microsoft Modern Work</h2>
+    <h2>Cloud Solutions</h2>
+    <h2>Microsoft End-User Training</h2>
+    <h2>Hardware Sales</h2>
+    <h2>Networking</h2>
+    <h2>CCTV</h2>
+    </>
+  )
+}
