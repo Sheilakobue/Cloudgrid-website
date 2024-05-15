a674f129417c3d5627ca0363b7d5d1a5b5ab799e
Sheila Kobue <Maserole.mojela@gmail.com>
2024-05-15 09:33:58 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,37 @@
-import type {Metadata} from "next";
-import {Poppins} from "next/font/google";
+import {useClient} from "use/client";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import Navbar from "./navbar/page";
 
-const poppins = Poppins({subsets: ["latin"], 
-weight: [
-  "100",
-  "200",
-  "300",
-  "400",
-  "500",
-  "600",
-  "700",
-  "800",
-]});
+
+useClient();
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
 export const metadata: Metadata = {
-  title: 'Cloudgrid Website',
-  description: 'Empowering businesses through cutting-edge technology solutions for streamlined operations and enhanced productivity',
-}
+  title: "Cloudgrid",
+  description:
+    "Empowering businesses through cutting-edge technology solutions for streamlined operations and enhanced productivity",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className ={poppins.className}>
-          <Navbar/>
-        {children}</body>
+      <head>
+      <link rel="icon" type="image/svg+xml" href="/public/logo.jpg" />
+     
+      </head>
+      <body className={poppins.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
