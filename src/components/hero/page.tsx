9f6a7e795ebadd318b69142c95c45e7d79b5032a
Sheila Kobue <Maserole.mojela@gmail.com>
2024-05-27 16:01:21 +0200

    adjusted pictures

--- src/components/hero/page.tsx
+++ src/components/hero/page.tsx
@@ -4,12 +4,12 @@ import { heroImage, logo } from "../../../public";
 
 export default function Hero() {
   return (
-    <div className="w-full h-[350px] flex items-center overflow-hidden">
+    <div className="w-full h-[310px] flex items-center overflow-hidden relative">
       <div className="w-full h-full  m-auto flex justify-center items-center relative">
         <div className="object-cover">
           <Image src={heroImage} height="100" width="1400" alt="banner" />
         </div>
-        <div className="absolute mt-[245px]">
+        <div className="absolute mt-[255px]">
           <Image src={logo} height="30" width="200" alt="Logo" />
         </div>
       </div>
