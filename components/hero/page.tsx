ae750fef9e5d33391d6a8ce8d49100a373a35656
Sheila Kobue <Maserole.mojela@gmail.com>
2024-05-27 11:36:34 +0200

    adjusted my herosecction

--- components/hero/page.tsx
+++ components/hero/page.tsx
@@ -1,17 +1,26 @@
 import Image from "next/image";
 import React from "react";
-import { heroImage } from "../../public";
+import { heroImage, logo } from "../../public";
 
 export default function Hero() {
   return (
-    <div className="w-full h-[600px] flex items-center">
-      <div className="h-full md:max-w-[1024px] max-w-[600px] m-auto flex justify-center items-center">
+    <div className="w-full h-[450px] flex items-center">
+      <div className="h-full md:max-w-[1024px] max-w-[600px] m-auto flex justify-center items-center relative">
         <div className="object-cover">
           <Image 
-          src={heroImage} 
-          height="100" 
-          width="1400" 
-          alt="banner" />
+              src={heroImage} 
+              height="100" 
+              width="1400" 
+              alt="banner" 
+              />
+        </div>
+        <div className="absolute mt-[322px]">
+          <Image 
+              src={logo} 
+              height="50" 
+              width="400" 
+              alt="Logo" 
+              />
         </div>
       </div>
     </div>
