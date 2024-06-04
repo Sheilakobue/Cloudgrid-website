5f0bee1eea3b4d07e0bd54b958a5609ec3bc69e8
Sheila Kobue <Maserole.mojela@gmail.com>
2024-06-04 14:50:33 +0200

    Created an about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 export default function About() {
   return (
-    <section className="relative w-full h-screen bg-blue-100">
+    <section className="relative w-full h-screen dark:text-neutral-100 dark:bg-gray-800">
     <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-gray-800">
       <h1 className="text-4xl md:text-6xl font-bold mb-4">About Us</h1>
-      <p className="text-xl md:text-2xl mb-8">Coming soon</p>
+      <p className="text-xs mb-4">
+            We are a proudly South African company, committed to empowering local businesses and driving economic progress. Our mission is to provide cutting-edge IT solutions, support, and services that help our clients succeed and thrive in an ever-changing technology landscape.
+          </p>
+          <p className="text-xs mb-4">
+            At CloudGrid, we prioritize your business needs, offering flexible support options, including onsite and remote assistance. We ensure transparency and affordability, providing competitive pricing, accurate feedback, and detailed reports. Our expertise includes thorough software audits and preventative maintenance, ensuring optimal system performance. With a proven track record, we're a trusted partner dedicated to helping your business thrive.
+          </p>
       </div>
       </section>
     
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,47 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Link from "next/link";
+import Image from "next/image";
+import Picture4 from "../../public/Picture4.png";
+import { HiArrowCircleDown } from "react-icons/hi";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      <div className=' dark:text-neutral-100 text-blue950  dark:bg-gray-800  '>
-      <h3>Who We Are</h3>
-      <p className="text-xs">Coming soon </p>
-      <Link href="/solutions">
-      <button className="text-xs px-1 py-1 bg-blue950 bg-opacity-50 text-white  hover:text-amber-500 transition duration-300 ease-in-out">Learn more</button>
-
-      </Link>
+      <div className="dark:text-neutral-100 text-blue-950 dark:bg-gray-800 flex flex-col text-center justify-center my-1 py-8 sm:py-10 md:flex-row md:space-x-4 md:py-30">
+        {/* Text section */}
+        <div className="md:mt-2 md:w-3/5">
+          <h3 className="text-lg font-bold text-amber-500 mt-0 md:text-left">Who We Are</h3>
+          <p className="text-xs mb-6 md:text-mt-2 md:text-left">
+            Welcome to CloudGrid, a proud Level 1 B-BBEE contributor committed
+            to delivering exceptional IT support services tailored to your
+            business needs. As a registered Microsoft partner, we specialize in
+            providing and supporting office software solutions ideal for small
+            and medium-sized enterprises.
+          </p>
+        </div>
+        {/* Image Button section */}
+        <div className="md:mt-8 mt-6 md:w-3/5 relative top-50  flex items-center justify-center">
+          <a href="/solutions" className="relative">
+            <Image
+              src={Picture4}
+              alt="solutions"
+              width={100}
+              height={100}
+              className="w-full h-full object-cover"
+            />
+            <span className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-white font-bold text-xs">
+              solutions
+            </span>
+          </a>
+        </div>
+         {/* Arrow.section */}
+        <div  className="flex flex-row justify-center md:mt-8 mt-6">
+          <Link href="/about" passHref>
+            <HiArrowCircleDown size={35} />
+          </Link>
+        </div>
       </div>
     </div>
   );
