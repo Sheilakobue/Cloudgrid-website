344cf5eeb9f5ea94ac506e753809e1cb0d8b19b3
Sheila Kobue <Maserole.mojela@gmail.com>
2024-06-05 11:55:40 +0200

    adjusted the navigation bar

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 export default function ContactUs() {
   return (
-    <section className="relative w-full h-screen bg-blue-100">
-    <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-gray-800">
-      <h1 className="text-4xl md:text-6xl font-bold mb-4">Contact Us</h1>
-      <p className="text-xl md:text-2xl mb-8">Coming soon</p>
-      </div>
-      </section>
-    
+    <form>
+    <input type="text" placeholder='First Name'/>
+    <input type="text" placeholder='Last Name'/>
+    <input type="text" placeholder='Company Name'/>
+    <input type="text" placeholder='Email'/>
+    <input type="text" placeholder='Phone'/>
+    <textarea></textarea>
+    </form>   
   )
 
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,42 +22,43 @@ export default function Navbar() {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <header className="w-full mx-auto px-4  bg-blue50 shadow  top-0 z-50 sm:px-20  dark:bg-gray-800  dark:border-b dark:border-stone-400">
+    <header className="w-full mx-auto px-4  bg-blue50 shadow  top-0 z-50 sm:px-20 dark:bg-gray-800  dark:border-b dark:border-stone-400">
       <div className="justify-between md:items-center md:flex">
-        <div className="flex items-center justify-between py-4">
-          <div className="md:py-5 md:block">
-            <Link href="/" className="flex items-center">
-              <Image src="/logo 2.jpg" alt="Logo" width={100} height={50} />
-            </Link>
-          </div>
-          <div className="md:hidden text-blue950 hover:text-amber500 dark:text-neutral-100">
-            <button onClick={() => setNavbar(!navbar)}>
-              {navbar ? <IoMdClose size={25} /> : <IoMdMenu size={25} />}
-            </button>
+        <div>
+          <div className="flex items-center justify-between py-3">
+            <div className="md:py-5 md:block">
+              <Link href="/" className="flex items-center">
+                <Image src="/logo 2.jpg" alt="Logo" width={100} height={50} />
+              </Link>
+            </div>
+            <div className="md:hidden text-blue950 hover:text-amber500 dark:text-neutral-100">
+              <button onClick={() => setNavbar(!navbar)}>
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
+            </div>
           </div>
         </div>
-
-        <div
-          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+        <div>
+        <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
             navbar ? "block" : "hidden"
-          }`}
-        >
-          <div className={`md:hidden ${navbar ? "block" : "hidden"}`}>
-            <div className="flex flex-col items-center space-y-3 pb-3 mt-8">
-              {NAV_ITEMS.map((item, idx) => (
+          }`}>
+        <div className="item-center justify-center space-y-3 md:flex md:space-x-4 md:space-y-0 text-blue950 dark:text-amber500">
+        {NAV_ITEMS.map((item, idx) => (
                 <Link
                   key={idx}
                   href={item.page}
-                  className="block text-blue-900 hover:text-amber500 dark:text-neutral-100"
-                  onClick={() => setNavbar(false)}
+                  className="block md:inline-block text-blue-950 hover:text-amber-500 dark:text-neutral-100"
+                  onClick={() => setNavbar(!navbar)}
                 >
                   {item.label}
                 </Link>
               ))}
-               <ThemeSwitch />
+         <div className="flex items-center justify-between ">
+            <ThemeSwitch />
             </div>
-          </div>
         </div>
+        </div>
+      </div>
       </div>
     </header>
   );
