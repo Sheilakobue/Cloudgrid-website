e332aee8da31f486de83277fedab4d541655aa34
Sheila Kobue <Maserole.mojela@gmail.com>
2024-06-14 09:55:15 +0200

    add solutions

--- src/app/solutions/page.tsx
+++ src/app/solutions/page.tsx
@@ -1,21 +1,61 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
+import SolutionItem from "../../components/SolutionItem"; // Ensure the path is correct
 import MicrosoftModernWork from "../../../public/MicrosoftModernWork.png";
+import CloudSolutions from "../../../public/CloudSolutions.png";
+import Training from "../../../public/Training.png";
+import Networking from "../../../public/Networking.png";
+import HardwareSales from "../../../public/HardwareSales.png";
+import CCTV from "../../../public/CCTV.png";
 
 export default function SolutionList() {
   return (
     <section className="p-4 bg-white dark:bg-gray-800">
       <h1 className="text-center text-4xl font-bold mb-8 text-amber-500">
         Our Solutions
-        <hr className="w-6 h-1 mx-auto m-4 bg-sky-300 border-0 rounded"></hr>
       </h1>
-      <div className="flex flex-col space-y-28">
-        <div className="flex flex-col md:flex-row md:space-x-12">
-          <div className="md:w-1/2">
-           
-          </div>
-        </div>
+      <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+        <SolutionItem
+          href="/solutions/microsoft-modern-work"
+          imageSrc={MicrosoftModernWork}
+          imageAlt="Microsoft Modern Work"
+          title="Microsoft Modern Work"
+          description="With Microsoft 365, you get all the familiar productivity apps your employees use every day."
+        />
+        <SolutionItem
+          href="/solutions/cloud-solutions"
+          imageSrc={CloudSolutions}
+          imageAlt="Cloud Solutions"
+          title="Cloud Solutions"
+          description="We provide a cloud-based data solution in which data is stored and processed on remote servers, rather than on on-site servers."
+        />
+        <SolutionItem
+          href="/solutions/microsoft-end-user-training"
+          imageSrc={Training}
+          imageAlt="Microsoft End-User Training"
+          title="Microsoft End-User Training"
+          description="We provide end-user training for Microsoft products to ensure your team can use them effectively."
+        />
+        <SolutionItem
+          href="/solutions/networking"
+          imageSrc={Networking}
+          imageAlt="Networking"
+          title="Networking"
+          description="Servers, firewalls, hubs, switches, routers, and cabling."
+        />
+        <SolutionItem
+          href="/solutions/hardware-sales"
+          imageSrc={HardwareSales}
+          imageAlt="Hardware Sales"
+          title="Hardware Sales"
+          description="Computers: desktops, notebooks, tablets. Printers: peripherals and consumables."
+        />
+        <SolutionItem
+          href="/solutions/cctv"
+          imageSrc={CCTV}
+          imageAlt="CCTV"
+          title="CCTV"
+          description="Camera sales, cabling, remote access, and maintenance."
+        />
       </div>
     </section>
   );
--- src/components/SolutionItem.tsx
+++ src/components/SolutionItem.tsx
@@ -3,23 +3,22 @@ import Link from "next/link";
 import Image from "next/image";
 
 const SolutionItem = ({ href, imageSrc, imageAlt, title, description }) => (
-  <div className="relative flex flex-col items-center text-center bg-gray-100 dark:bg-gray-900 p-4 rounded-lg shadow-md">
+  <div className="relative flex flex-col items-center text-center bg-blue950 border-0 bg-opacity-50 dark:bg-gray-900 p-4 rounded-lg shadow-md">
     <Link href={href} passHref legacyBehavior>
-      <a className="relative w-full h-48 mb-4 cursor-pointer">
+      <a className="relative mb-4 cursor-pointer">
         <Image
           src={imageSrc}
           alt={imageAlt}
-          width={100}
-          height={100}
+          width={200}
+          height={180}
           className="w-full h-full object-cover cursor-pointer"
-          
-          loading="lazy"
+           loading="lazy"
         />
-        <span className="absolute flex items-center justify-center bg-black bg-opacity-50 text-white font-bold text-xl rounded-lg transition-opacity duration-300 hover:opacity-75">
-          {title}
-        </span>
       </a>
     </Link>
+    <span className="absolute inset-0 flex items-center justify-center text-white font-bold text-l hover:opacity-75">
+          {title}
+        </span>
     <p className="text-sm text-gray-700 dark:text-gray-300">{description}</p>
   </div>
 );
